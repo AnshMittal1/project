2caae60f484ebbfc7f72294c4b0a93d3de63a6c0
MolikRajvanshi <anshurajvanshi49@gmail.com>
2024-07-21 21:59:28 +0530

    added the footer page to our linkedIn

---
+++ script-linked.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const profile1PictureUrl = 'https://media.licdn.com/dms/image/D4E03AQH4xwF76RMI1g/profile-displayphoto-shrink_200_200/0/1700132078374?e=1727308800&v=beta&t=8hmNq3v6JlfdAd_eCZzzaKuCOQoY9J0qQR03SVUyLbQ';
+    const profile2PictureUrl = 'https://media.licdn.com/dms/image/D5603AQHVmaduHDOXbA/profile-displayphoto-shrink_200_200/0/1720375154755?e=1727308800&v=beta&t=fsJOxcbjSW5iucImVLNcCPmRDmimm-Vj_vZnU4rDvkU';
+
+    document.getElementById('profile1-picture').src = profile1PictureUrl;
+    document.getElementById('profile2-picture').src = profile2PictureUrl;
+});
